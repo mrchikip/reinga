5712c4f9035d69ca79bc8c3dc85375b7724b8f33
MrChikip <mrchikip@users.noreply.github.com>
2023-03-26 00:14:37 -0500

    configurado y probada la funcion de eliminar registros

--- src/routes/hardware.js
+++ src/routes/hardware.js
@@ -50,37 +50,53 @@ router.get("/", isLoggedIn, async (req, res) => {
   res.render("hardware/list", { hardware });
 });
 
-// router.get('/modules/hardware/delete/:id', isLoggedIn, async(req, res) => {
-//     const { id } = req.params;
-//     await pool.query('DELETE FROM equipos WHERE ID = ?', [id]);
-//     req.flash('success', 'Registro eliminado satisfactoriamente');
-//     res.redirect('modules/hardware/list');
+router.get('/delete/:serial', isLoggedIn, async(req, res) => {
+    const { serial } = req.params;
+    await pool.query('DELETE FROM equipos WHERE serial = ?', [serial]);
+    req.flash('success', 'Registro eliminado satisfactoriamente');
+    res.redirect('/hardware');
+});
+
+// router.get("/edit/:serial", isLoggedIn, async (req, res) => {
+//   const { serial } = req.params;
+//   const hardware = await pool.query("SELECT * FROM equipos WHERE serial = ?", [serial]);
+//   res.render("hardware/edit", { hardware: hardware[0] });
 // });
 
-// router.get('/hardware/edit/:id', isLoggedIn, async(req, res) => {
-//     const { id } = req.params;
-//     const hardware = await pool.query('SELECT * FROM equipos WHERE ID = ?', [id]);
-//     res.render('/modules/hardware/edit', { hardware: hardware[0] });
-// });
-
-// router.post('/hardware/edit/:id', isLoggedIn, async(req, res) => {
-//     const { id } = req.params;
-//     const { nombre_equipo, categoria, serial, sistema_operativo, modelo, fabricante, procesador, ram, tipo_almacenamiento, capacidad_almacenamiento } = req.body;
-//     const newRegistro = {
-//         nombre_equipo,
-//         categoria,
-//         serial,
-//         sistema_operativo,
-//         modelo,
-//         fabricante,
-//         procesador,
-//         ram,
-//         tipo_almacenamiento,
-//         capacidad_almacenamiento
-//     };
-//     await pool.query('UPDATE equipos set ? WHERE ID = ?', [newRegistro, id]);
-//     req.flash('success', 'Registro actualizado satisfactoriamente');
-//     res.redirect('/src/views/modules/hardware/list.hbs');
+// router.post("/edit/:serial", isLoggedIn, async (req, res) => {
+//   const { serial } = req.params;
+//   const {
+//     nombre_equipo,
+//     categoria,
+//     sistema_operativo,
+//     modelo,
+//     fabricante,
+//     procesador,
+//     ram,
+//     tipo_almacenamiento,
+//     capacidad_almacenamiento,
+//     propiedad,
+//     salud,
+//     fecha_ultimo_mantenimiento,
+//   } = req.body;
+//   const newRegistro = {
+//     serial,
+//     nombre_equipo,
+//     categoria,
+//     sistema_operativo,
+//     modelo,
+//     fabricante,
+//     procesador,
+//     ram,
+//     tipo_almacenamiento,
+//     capacidad_almacenamiento,
+//     propiedad,
+//     salud,
+//     fecha_ultimo_mantenimiento,
+//   };
+//   await pool.query("UPDATE equipos set ? WHERE serial = ?", [newRegistro, serial]);
+//   req.flash("success", "Registro actualizado satisfactoriamente");
+//   res.redirect("/hardware");
 // });
 
 module.exports = router;
