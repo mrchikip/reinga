04f1052060fa475da15c7ed89c5b5dab98cb35cd
MrChikip <mrchikip@users.noreply.github.com>
2023-03-25 22:30:03 -0500

    creado el boton de agregar nuevo registro y las correspondientes pruebas

--- src/routes/hardware.js
+++ src/routes/hardware.js
@@ -1,37 +1,53 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const pool = require('../database');
-const { isLoggedIn } = require('../lib/auth');
+const pool = require("../database");
+const { isLoggedIn } = require("../lib/auth");
 
+router.get("/add", (req, res) => {
+  res.render("hardware/add");
+});
 
-// router.get('/hardware/add', (req, res) => {
-//     res.render('modules/hardware/add');
-// });
+router.post("/add", isLoggedIn, async (req, res) => {
+  const {
+    nombre_equipo,
+    categoria,
+    serial,
+    sistema_operativo,
+    modelo,
+    fabricante,
+    procesador,
+    ram,
+    tipo_almacenamiento,
+    capacidad_almacenamiento,
+    propiedad,
+    salud,
+    fecha_ultimo_mantenimiento,
+  } = req.body;
+  const newRegistro = {
+    nombre_equipo,
+    categoria,
+    serial,
+    sistema_operativo,
+    modelo,
+    fabricante,
+    procesador,
+    ram,
+    tipo_almacenamiento,
+    capacidad_almacenamiento,
+    propiedad,
+    salud,
+    fecha_ultimo_mantenimiento,
+  };
 
-// router.post('/hardware/add', isLoggedIn, async(req, res) => {
-//     const { nombre_equipo, categoria, serial, sistema_operativo, modelo, fabricante, procesador, ram, tipo_almacenamiento, capacidad_almacenamiento } = req.body;
-//     const newRegistro = {
-//         nombre_equipo,
-//         categoria,
-//         serial,
-//         sistema_operativo,
-//         modelo,
-//         fabricante,
-//         procesador,
-//         ram,
-//         tipo_almacenamiento,
-//         capacidad_almacenamiento
-//     };  
-//     await pool.query('INSERT INTO equipos set ?', [newRegistro]);
-//     req.flash('success', 'Registro guardado satisfactoriamente');
-//     res.redirect('/modules/hardware/list');
-// });
+  await pool.query("INSERT INTO equipos set ?", [newRegistro]);
+  req.flash("success", "Registro guardado satisfactoriamente");
+  res.redirect("/hardware");
+});
 
-
-router.get('/', isLoggedIn, async(req, res) => {
-    const hardware = await pool.query('SELECT * FROM equipos');
-    res.render('hardware/list', { hardware });
+router.get("/", isLoggedIn, async (req, res) => {
+  const hardware = await pool.query("SELECT * FROM equipos");
+  res.render("hardware/list", { hardware });
 });
 
 // router.get('/modules/hardware/delete/:id', isLoggedIn, async(req, res) => {
@@ -61,7 +77,7 @@ router.get('/', isLoggedIn, async(req, res) => {
 //         ram,
 //         tipo_almacenamiento,
 //         capacidad_almacenamiento
-//     };  
+//     };
 //     await pool.query('UPDATE equipos set ? WHERE ID = ?', [newRegistro, id]);
 //     req.flash('success', 'Registro actualizado satisfactoriamente');
 //     res.redirect('/src/views/modules/hardware/list.hbs');
