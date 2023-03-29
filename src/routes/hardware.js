e61d2170e3ce14f6431ce32a28128e6fdf25aa11
MrChikip <mrchikip@users.noreply.github.com>
2023-03-29 00:33:18 -0500

    se agrega boton de busqueda y se descarta barra de busqueda en list.hbs

--- src/routes/hardware.js
+++ src/routes/hardware.js
@@ -8,6 +8,10 @@ router.get("/add", isLoggedIn, (req, res) => {
   res.render("hardware/add");
 });
 
+router.get("/search", isLoggedIn, (req, res) => {
+  res.render("hardware/search");
+});
+
 router.post("/add", isLoggedIn, async (req, res) => {
   const { nombre_equipo, categoria, serial, sistema_operativo, modelo, fabricante, procesador, ram, tipo_almacenamiento, capacidad_almacenamiento, propiedad, salud, fecha_ultimo_mantenimiento } = req.body;
   const newRegistro = {
@@ -70,5 +74,17 @@ router.post('/edit/:serial', isLoggedIn, async (req, res) => {
   req.flash('success', 'Registro actualizado satisfactoriamente');
   res.redirect('/hardware');
 });
+
+router.get('/equipo/:serial', isLoggedIn, async(req, res) => {
+  const { buscarSerial } = req.body;
+  const equipo = await pool.query('SELECT * FROM equipos WHERE serial = ?', [serial]);
+  if (equipo.length > 0) {
+    res.render('hardware/equipo', { equipo: equipo[0] });
+  } else {
+    req.flash('message', 'Equipo no encontrado');
+    res.redirect('/hardware');
+  }
+});
+
 
 module.exports = router;
