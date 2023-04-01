7af86c07222acb75463be40aa6772b9f8e78490a
MrChikip <mrchikip@users.noreply.github.com>
2023-04-01 00:00:38 -0500

    inicio de la creacion de las rutas para people

--- src/routes/people.js
+++ src/routes/people.js
@@ -1,8 +1,61 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-router.get('/',(req, res) => {
-    res.render('people/list');
+const pool = require("../database");
+const { isLoggedIn } = require("../lib/auth");
+
+router.get("/", isLoggedIn, async (req, res) => {
+  const people = await pool.query("SELECT * FROM usuarios");
+  res.render("people/list", { people });
 });
+
+router.get("/add", isLoggedIn, (req, res) => {
+  res.render("people/add");
+});
+
+router.post("/add", isLoggedIn, async (req, res) => {
+  const { nombre_empleado, usuario, cedula, cargo, proceso, centro_costo } = req.body;
+  const newRegistro = {
+    nombre_empleado,
+    usuario,
+    cedula,
+    cargo,
+    proceso,
+    centro_costo,
+  };
+
+  await pool.query("INSERT INTO equipos SET ?", [newRegistro]);
+  req.flash("success", "Registro guardado satisfactoriamente");
+  res.redirect("/hardware");
+});
+
+router.post('/search',isLoggedIn, async (req, res) => {
+    const busqueda = req.body.busqueda;
+    const persona = await pool.query(`SELECT * FROM usuarios WHERE cedula LIKE '%${busqueda}%'`);
+  res.render('people/search', { persona });
+  });
+
+  router.get("/edit/:cedula", isLoggedIn, async (req, res) => {
+    const { cedula } = req.params;
+    const persona = await pool.query('SELECT * FROM usuarios WHERE cedula = ?', [cedula]);
+    res.render('people/edit', { persona: persona[0] });
+  });
+  
+  router.post('/edit/:cedula', isLoggedIn, async (req, res) => {
+    const { cedula } = req.params;
+    const { nombre_empleado, usuario, cargo, proceso, centro_costo } = req.body;
+    const editRegistro = {
+        nombre_empleado,
+        usuario,
+        cedula,
+        cargo,
+        proceso,
+        centro_costo,
+      };
+    await pool.query('UPDATE usuarios SET ? WHERE cedula = ?', [editRegistro, cedula]);
+    req.flash('success', 'Registro actualizado satisfactoriamente');
+    res.redirect('/people');
+  });
+  
 
 module.exports = router;
