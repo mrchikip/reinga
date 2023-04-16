fefe6d892734250cac20078e3403e239a5088d12
MrChikip <mrchikip@users.noreply.github.com>
2023-04-16 02:35:16 -0500

    codeado el apartado delete y edit, finalizado el crud del modulo printers

--- src/routes/printers.js
+++ src/routes/printers.js
@@ -47,60 +47,39 @@ router.post("/add", isLoggedIn, async (req, res) => {
   res.redirect("/printers");
 });
 
-// router.get("/edit/:serial", isLoggedIn, async (req, res) => {
-//   const { serial } = req.params;
-//   const assignation = await pool.query("SELECT nombre_empleado FROM usuarios");
-//   const hardware = await pool.query("SELECT * FROM equipos WHERE serial = ?", [
-//     serial,
-//   ]);
-//   res.render("hardware/edit", { assignation, hardware: hardware[0] });
-// });
+router.get("/edit/:serial", isLoggedIn, async (req, res) => {
+  const { serial } = req.params;
+  const printer = await pool.query("SELECT * FROM printers WHERE serial = ?", [
+    serial,
+  ]);
+  res.render("printers/edit", { printer: printer[0] });
+});
 
-// router.post("/edit/:serial", isLoggedIn, async (req, res) => {
-//   const { serial } = req.params;
-//   const {
-//     nombre_equipo,
-//     categoria,
-//     sistema_operativo,
-//     modelo,
-//     fabricante,
-//     procesador,
-//     ram,
-//     tipo_almacenamiento,
-//     capacidad_almacenamiento,
-//     assignation,
-//     propiedad,
-//     salud,
-//     fecha_ultimo_mantenimiento,
-//   } = req.body;
-//   const editRegistro = {
-//     nombre_equipo,
-//     categoria,
-//     sistema_operativo,
-//     modelo,
-//     fabricante,
-//     procesador,
-//     ram,
-//     tipo_almacenamiento,
-//     capacidad_almacenamiento,
-//     assignation,
-//     propiedad,
-//     salud,
-//     fecha_ultimo_mantenimiento,
-//   };
-//   await pool.query("UPDATE equipos SET ? WHERE serial = ?", [
-//     editRegistro,
-//     serial,
-//   ]);
-//   req.flash("success", "Registro actualizado satisfactoriamente");
-//   res.redirect("/hardware");
-// });
+router.post("/edit/:serial", isLoggedIn, async (req, res) => {
+  const { serial } = req.params;
+  const { type, marca, modelo, ubicacion, propiety, estado } = req.body;
+  const editRegistro = {
+    serial,
+    type,
+    marca,
+    modelo,
+    ubicacion,
+    propiety,
+    estado,
+  };
+  await pool.query("UPDATE printers SET ? WHERE serial = ?", [
+    editRegistro,
+    serial,
+  ]);
+  req.flash("success", "Registro actualizado satisfactoriamente");
+  res.redirect("/printers");
+});
 
-// router.get("/delete/:serial", isLoggedIn, async (req, res) => {
-//   const { serial } = req.params;
-//   await pool.query("DELETE FROM equipos WHERE serial = ?", [serial]);
-//   req.flash("success", "Registro eliminado satisfactoriamente");
-//   res.redirect("/hardware");
-// });
+router.get("/delete/:serial", isLoggedIn, async (req, res) => {
+  const { serial } = req.params;
+  await pool.query("DELETE FROM printers WHERE serial = ?", [serial]);
+  req.flash("success", "Registro eliminado satisfactoriamente");
+  res.redirect("/printers");
+});
 
 module.exports = router;
